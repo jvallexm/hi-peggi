2934ad4e8fb0a6eedc1c109ba4dea3db4116f5ec
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2022-02-18 14:38:16 -0500

    Now capitalizes lines

--- public/src/index.js
+++ public/src/index.js
@@ -1,7 +1,18 @@
 $("#main").on("input", e => {
     let data = e.target.value
     let newData = data.replace(/Yellow\shighlight\s\|\s?Location\:\s[0-9,]+\n/g, "") //.replace(/\n\n/g, "")
-    $("#output").text(newData)
+    let output = [];
+    let outputData = newData.split("\n")
+    outputData.forEach(line => {
+        if(line.match(/^[a-z].+/g)) {
+            let splitLine = line.split("");
+            splitLine[0] = splitLine[0].toUpperCase();
+            output.push(splitLine.join(""));
+        } else {
+            output.push(line);
+        }
+    })
+    $("#output").text(output.join("\n"))
 })
 $("#output").on("input", e => {
     e.preventDefault();
