4c297252d9d17f2a98b2087f640f286a94bdcb3d
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2022-02-18 14:29:16 -0500

    hot fix

--- public/src/index.js
+++ public/src/index.js
@@ -1,6 +1,6 @@
 $("#main").on("input", e => {
     let data = e.target.value
-    let newData = data.replace(/Yellow\shighlight\s\|\s?Location\:\s[0-9,]+\n/g, "").replace(/\n\n/g, "")
+    let newData = data.replace(/Yellow\shighlight\s\|\s?Location\:\s[0-9,]+\n/g, "") //.replace(/\n\n/g, "")
     $("#output").text(newData)
 })
 $("#output").on("input", e => {
