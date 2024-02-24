31cecb195ef70beadbe91aea9ef067693749cb97
jvallexm <jvallexm@gmail.com>
2024-02-23 19:58:16 -0500

    prettier

--- public/src/index.js
+++ public/src/index.js
@@ -1,19 +1,22 @@
-$("#main").on("input", e => {
-    let data = e.target.value
-    let newData = data.replace(/Yellow\shighlight\s\|\s?(Location|Page)\:\s[0-9,]+\n/g, "") //.replace(/\n\n/g, "")
-    let output = [];
-    let outputData = newData.split("\n")
-    outputData.forEach(line => {
-        if(line.match(/^[a-z].+/g)) {
-            let splitLine = line.split("");
-            splitLine[0] = splitLine[0].toUpperCase();
-            output.push(splitLine.join(""));
-        } else {
-            output.push(line);
-        }
-    })
-    $("#output").text(output.join("\n"))
-})
-$("#output").on("input", e => {
-    e.preventDefault();
-})
+$("#main").on("input", (e) => {
+  let data = e.target.value;
+  let newData = data.replace(
+    /Yellow\shighlight\s\|\s?(Location|Page)\:\s[0-9,]+\n/g,
+    ""
+  ); //.replace(/\n\n/g, "")
+  let output = [];
+  let outputData = newData.split("\n");
+  outputData.forEach((line) => {
+    if (line.match(/^[a-z].+/g)) {
+      let splitLine = line.split("");
+      splitLine[0] = splitLine[0].toUpperCase();
+      output.push(splitLine.join(""));
+    } else {
+      output.push(line);
+    }
+  });
+  $("#output").text(output.join("\n"));
+});
+$("#output").on("input", (e) => {
+  e.preventDefault();
+});
